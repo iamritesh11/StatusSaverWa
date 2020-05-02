def2a5e55a676971eaf2db1ca4e52cb1f3f4ec83
iamritesh11 <rkb11ritesh@gmail.com>
2020-05-02 17:48:11 +0530

    Whatsapp status saver

---
+++ app/src/main/java/com/statussaver/ItemOneFragment.java
@@ -0,0 +1,100 @@
+package com.statussaver;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mukesh.countrypicker.Country;
+import com.mukesh.countrypicker.CountryPicker;
+import com.mukesh.countrypicker.OnCountryPickerListener;
+
+import com.rawderm.whatsapptools.R;
+
+public class ItemOneFragment extends Fragment {
+    private EditText editno, editmsg;
+    private Button button, codeButton;
+    private String url, msg;
+    private View myView;
+
+    public static ItemOneFragment newInstance() {
+        ItemOneFragment fragment = new ItemOneFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        myView = inflater.inflate(R.layout.fragment_item_one, container, false);
+        editno = myView.findViewById(R.id.editno);
+        editmsg = myView.findViewById(R.id.editmsg);
+        button = (Button)myView.findViewById(R.id.button);
+        codeButton =(Button) myView.findViewById(R.id.codeButton);
+        getActivity().setTitle("WhatsApp Messaging");
+
+        codeButton.setBackgroundColor(Color.TRANSPARENT);
+
+        codeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CountryPicker countryPicker =
+                        new CountryPicker.Builder().with(getActivity())
+                                .listener(new OnCountryPickerListener() {
+                                    @Override public void onSelectCountry(Country country) {
+                                        //DO something here
+                                        String dialCode = country.getDialCode();
+                                        codeButton.setText(dialCode);
+                                    }
+                                })
+                                .build();
+                countryPicker.showDialog(getFragmentManager());// Show the dialog
+
+            }
+        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                msg = editmsg.getText().toString();
+                //msg.replaceAll(" ", "%20");
+                //msg = Html.fromHtml(msg).toString();
+                //msg = TextUtils.htmlEncode(msg);
+                url = editno.getText().toString();
+                if (url.isEmpty()){
+                    Toast.makeText(getActivity(), "Please enter whatsapp number!", Toast.LENGTH_SHORT).show();
+                }
+                else if (url.length()<8){
+                    Toast.makeText(getActivity(), "Wrong number maybe!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    url = "https://api.whatsapp.com/send?phone="+codeButton.getText() + url + "&text=" + msg;
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    try{
+                        startActivity(intent);
+                        intent.setPackage("com.whatsapp");
+                    }
+                    catch (Exception e){
+                        Toast.makeText(getActivity(), "Whatsapp not installed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+
+        return myView;
+    }
+}
